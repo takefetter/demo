5bb8161e5e45e8393fad588e4b91f09cb8444c89
takefetter <takefetter@gmail.com>
2018-03-31 10:55:57 +0800

    Python Solution 2018.3.31

---
+++ algorithm/Python/排序/插入排序.py
@@ -0,0 +1,12 @@
+def insert_sort(lists):
+    # 插入排序
+    count = len(lists)
+    for i in range(1, count):
+        key = lists[i]
+        j = i - 1
+        while j >= 0:
+            if lists[j] > key:
+                lists[j + 1] = lists[j]
+                lists[j] = key
+            j -= 1
+    return lists
